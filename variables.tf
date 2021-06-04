6da964303db0ba19ee3ef602f0961d037be7ec6f
Tran Vi Hung <55351898+vihungtran13@users.noreply.github.com>
2021-06-04 14:28:12 +0700

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  default     = ""
+  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
+}
+
+variable "namespace" {
+  default     = ""
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
+}
+
+variable "source_instance_id" {}
+
+variable "snapshot_without_reboot" {
+  type = bool
+  default = true
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
+}
